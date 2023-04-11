eda6e2383917882c5983b528d595d0e9b6252ba4
calebeLAR <ca6ele@gmail.com>
2023-04-10 21:21:10 -0400

    index.js

--- index.js
+++ index.js
@@ -3,42 +3,10 @@ const {Sequelize, DataTypes} = require('sequelize');
 const sequelize = new Sequelize('sequelize', 'root', 'root', {
   host: 'localhost',
   port: 3306,
-  dialect: 'mysql',
-  define: {
-    freezeTableName: true,
-    timestamps: false
-  }
+  dialect: 'mysql'
 }); 
 
-const User = sequelize.define('users', {
-  user_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: DataTypes.CHAR(30),
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  age: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  is_active: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
-  }
-});
-
-const Games = sequelize.define('games', {
+const Games = sequelize.define('game', {
   game_id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
@@ -48,25 +16,73 @@ const Games = sequelize.define('games', {
     type: DataTypes.CHAR(30),
     allowNull: false
   },
-  size: {
-    type: DataTypes.TINYINT,
+  mb_size: {
+    type: DataTypes.DECIMAL(12,2),
     allowNull: false,
   },
   price: {
-    type: DataTypes.CHAR(5),
+    type: DataTypes.CHAR(8),
     defaultValue: 'free'
   },
   realease: {
     type: DataTypes.DATE,
   }
+},{
+  timestamps: false
 });
 
-async function activeModel (Model) {
-  await Model.sync({force: true})
-  console.log('sucesso')
-};
+// Games.sync({force: true}).then(()=> {
+//   const game = Games.build({name: 'teste1', mb_size: 18000 })
+//   game.save()
+//   console.log(game.toJSON())
+// });
 
-activeModel(User);
-activeModel(Games);
+// Games.sync().then(()=> {
+//   const game = Games.create({name: 'create3', mb_size: 18.01 })
+//   return game
+// }).then((game)=> {
+//   console.log(game.toJSON())
+// });
 
-// sequelize.authenticate().then(()=> console.log('sucesso')).catch(err => console.log('fracasso'));
+// Games.sync({force:true}).then(()=> {
+//   const game1 = Games.create({name: 'create1', mb_size: 18.00 })
+//   const game2 = Games.create({name: 'create2', mb_size: 17.41 })
+//   const game3 = Games.create({name: 'create3', mb_size: 180.50 })
+//   const game4 = Games.create({name: 'create4', mb_size: 15.00 })
+//   return Promise.all([game1, game2, game3, game4])
+// }).then((games)=> {
+//   const game3 = games.find((game)=> game.dataValues.name === 'create3');
+//   return game3.destroy();
+// }).then((game3)=>{
+//   console.log(Games, 'destroyed')
+// });
+
+// Games.sync({alter: true}).then(()=> {
+//   const game = Games.create({name: 'teste4', mb_size: 18000 })
+//   return game
+// }).then((game) => {
+//   game.name = 'reload'
+//   console.log(game.name)
+//   return game.reload()
+// }).then((gameReload)=>{
+//   console.log(gameReload.toJSON())
+// });
+
+
+
+// Games.sync().then(()=> {
+//   const game1 = Games.create({name: 'create1', mb_size: 18.00 })
+//   const game2 = Games.create({name: 'create2', mb_size: 17.41 })
+//   const games = Promise.all([game1, game2])
+//   return games
+// }).then((games)=> {
+//   const game2 = games.find((game)=> game.dataValues.name === 'create2')
+//   game2.mb_size = 3000.45;
+//   game2.save({fields: ['mb_size']})
+//   return game2
+// }).catch((err)=>{
+//   console.log(err)
+// });
+
+
+
